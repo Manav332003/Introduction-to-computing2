9fa77df507d62cf186cd6d8b9a38daef4243561f
Manav332003 <97628752+Manav332003@users.noreply.github.com>
2022-01-25 21:48:16 +0530

    Add files via upload

---
+++ output2_21105107.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:\Users\Manav Goyal\OneDrive\Desktop\assignment2_21105107.txt ===
+#Output of question1
+Length of string: 35
+ Reverse order of string: egaugnal evitisnes esac a si nohtyP
+New String: a case sensitive
+D: Python is object oriented language
+Index of 'a' is= 10
+String without space: Pythonisacasesensitivelanguage
+
+#Output of question2
+Enter your name:Manav 
+Enter your SID:21105107
+Department:ECE
+CGPA:9.0
+Hey,Manav  Here!
+ My SID is 21105107
+I am from ECEdepartment
+ and my CGPA is 9.000000
+
+#Output of question3
+The value of a&b: 8
+The value of a|b: 58
+The value of a^b: 50
+The value of a<<2 and b<<2: (224, 40)
+The value of a>>2 and b>>4: (14, 0)
+
+
+#Output of question4
+Enter number1:25
+Enter number2:10
+Enter number3:45
+Greatest number is num3
+
+
+#Output of question5
+Enter the string:My name is Manav Goyal
+Yes
+
+
+#Output of question6
+side1: 2
+side2: 4
+side3: 5
+Yes
